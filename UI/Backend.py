1d6b78fefd498002635081085514ad4829fa0d85
Jacob Abraham <jacobabraham7981@gmail.com>
2021-03-30 18:03:46 +0530

    alternative for Nuscenes-devkit

--- UI/Backend.py
+++ UI/Backend.py
@@ -2,7 +2,8 @@
 import socket
 import time
 import math
-from nuscenes.nuscenes import NuScenes 
+#from nuscenes.nuscenes import NuScenes 
+import json
 import numpy as np
 from pyquaternion import Quaternion
 
@@ -11,7 +12,7 @@ host, port = "127.0.0.1", 54955
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((host, port))
 
-nusc = NuScenes(version='v1.0-mini', dataroot='data', verbose=True)
+#nusc = NuScenes(version='v1.0-mini', dataroot='data', verbose=True)
 
 max_height=5
 max_length=20
@@ -51,54 +52,49 @@ def convert_to_top_corner(point):
       point[1] = max_width - point[1]
       return point
 
-for scn in nusc.scene:
-    my_sample = nusc.get('sample', scn['first_sample_token'])
-    while my_sample['next'] != '':
-        sensor = 'LIDAR_TOP'
-        lidar_top_data = nusc.get('sample_data', my_sample['data'][sensor])
-        ego_pose = nusc.get('ego_pose', lidar_top_data['ego_pose_token'])
-        sock.sendall("FRAME".encode("UTF-8"))
-        first_annos_flag = True
-        for annos in my_sample['anns']:
-            annotation_metadata =  nusc.get('sample_annotation', annos)
-            
-            ego_yaw = quaternion_yaw(ego_pose['rotation']) - math.pi/2
-            cordinates = [annotation_metadata['translation'][i] - ego_pose['translation'][i] for i in range(3)]
-            cordinates[0], cordinates[1] = rotate_around_point_lowperf(cordinates[:2], ego_yaw, origin=(0, 0))
-            #cordinates = convert_to_top_corner(cordinates)
-            if cordinates[0] > max_width or cordinates[0] < - max_width or cordinates[1] > max_length or cordinates[1] < -max_length:# or (self.augment and self.check_cameraregion() == 0):
-                continue
 
-            rotation = [annotation_metadata['rotation'][i] - ego_pose['rotation'][i] for i in range(4)]
+with open('data/combined_samples.json') as f:
+    samples = json.load(f)
+for i in range(394):
+    ego_pose = samples[i]['ego_pose']
+    sock.sendall("FRAME".encode("UTF-8"))
+    first_annos_flag = True
+    for annos in samples[i]['anns']:
+        annotation_metadata =  nusc.get('sample_annotation', annos)
+        
+        ego_yaw = quaternion_yaw(ego_pose['rotation']) - math.pi/2
+        cordinates = [annotation_metadata['translation'][i] - ego_pose['translation'][i] for i in range(3)]
+        cordinates[0], cordinates[1] = rotate_around_point_lowperf(cordinates[:2], ego_yaw, origin=(0, 0))
+        #cordinates = convert_to_top_corner(cordinates)
+        if cordinates[0] > max_width or cordinates[0] < - max_width or cordinates[1] > max_length or cordinates[1] < -max_length:# or (self.augment and self.check_cameraregion() == 0):
+            continue
 
-            #converting list to string then to byte and sending to c#
-            transString = ','.join(map(str, cordinates)) #Converting translation list to a string, example "0,0,0"
-            sizeString = ','.join(map(str, annotation_metadata['size'])) #Converting size list to a string, example "0,0,0"
-            rotationString = ','.join(map(str, rotation)) #Converting rotation list to a string, example "0,0,0"
-            
-            #Converting string to Byte, and sending it to C#
-            if first_annos_flag == False:
-                sock.sendall("NOPE".encode("UTF-8"))
-            first_annos_flag = False
-            time.sleep(0.0001)
-            sock.sendall(sizeString.encode("UTF-8")) 
-            time.sleep(0.0001)
-            sock.sendall(rotationString.encode("UTF-8")) 
-            time.sleep(0.0001)
-            sock.sendall(annotation_metadata['category_name'].encode("UTF-8")) 
-            time.sleep(0.0001)
-            sock.sendall(transString.encode("UTF-8")) 
-            time.sleep(0.0001)
-            print(transString,sizeString,rotationString,annotation_metadata['category_name'])
+        rotation = [annotation_metadata['rotation'][i] - ego_pose['rotation'][i] for i in range(4)]
 
-            #receivedData = sock.recv(1024).decode("UTF-8") #receiveing data in Byte fron C#, and converting it to String
-            #print(receivedData)
-        my_sample = nusc.get('sample', my_sample['next'])
-        time.sleep(0.48) #sleep 0.5 sec
-        sock.sendall("DONE".encode("UTF-8")) #please delete this for final testing
-        break #use this break while testing
-    #sock.sendall("SCENE".encode("UTF-8"))
+        #converting list to string then to byte and sending to c#
+        transString = ','.join(map(str, cordinates)) #Converting translation list to a string, example "0,0,0"
+        sizeString = ','.join(map(str, annotation_metadata['size'])) #Converting size list to a string, example "0,0,0"
+        rotationString = ','.join(map(str, rotation)) #Converting rotation list to a string, example "0,0,0"
+        
+        #Converting string to Byte, and sending it to C#
+        if first_annos_flag == False:
+            sock.sendall("NOPE".encode("UTF-8"))
+        first_annos_flag = False
+        time.sleep(0.0001)
+        sock.sendall(sizeString.encode("UTF-8")) 
+        time.sleep(0.0001)
+        sock.sendall(rotationString.encode("UTF-8")) 
+        time.sleep(0.0001)
+        sock.sendall(annotation_metadata['category_name'].encode("UTF-8")) 
+        time.sleep(0.0001)
+        sock.sendall(transString.encode("UTF-8")) 
+        time.sleep(0.0001)
+        print(transString,sizeString,rotationString,annotation_metadata['category_name'])
+
+        #receivedData = sock.recv(1024).decode("UTF-8") #receiveing data in Byte fron C#, and converting it to String
+        #print(receivedData)
+    time.sleep(0.48) #sleep 0.5 sec
     sock.sendall("DONE".encode("UTF-8")) #please delete this for final testing
-    print("one scn done")
-    break
+    break #use this break while testing
+    #sock.sendall("SCENE".encode("UTF-8"))
 sock.sendall("DONE".encode("UTF-8"))
